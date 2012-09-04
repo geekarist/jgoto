b96a87c64bd578995d093f3f7577f20456fece78
Christophe Pelé <christophe.pele@gmail.com>
2012-09-04 20:21:43 +0200

    Code first unit test: get duration for going from one address to another in paris.

--- jgoto/src/main/java/com/github/geekarist/jgoto/App.java
+++ jgoto/src/main/java/com/github/geekarist/jgoto/App.java
@@ -1,13 +1,39 @@
 package com.github.geekarist.jgoto;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private String from;
+	private String to;
+	private String duration;
+
+	public App from(String string) {
+		from = string;
+		return this;
+	}
+
+	public App to(String string) {
+		to = string;
+		return this;
+	}
+
+	public String duration() throws IOException {
+		computeRoute();
+		return duration;
+	}
+
+	private void computeRoute() throws IOException {
+		Map<String, String> postData = new HashMap<String, String>();
+		postData.put("itineraire_start", from);
+		postData.put("itineraire_end", to);
+		Document doc = Jsoup.connect("http://www.ratp.fr/").data(postData).post();
+		duration = doc.html();
+	}
 }
--- jgoto/src/test/java/com/github/geekarist/jgoto/AppTest.java
+++ jgoto/src/test/java/com/github/geekarist/jgoto/AppTest.java
@@ -1,38 +1,19 @@
 package com.github.geekarist.jgoto;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import junit.framework.Assert;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@Test
+	public void testApp() throws IOException {
+		App app = new App();
+		String duration = app.from("9, Rue de la Croix Faubin, 75011, Paris")
+				.to("63, bd de Brandebourg, 94200, Ivry-sur-Seine").duration();
+		String msg = String.format("Incorrect duration [%s]", duration);
+		Assert.assertTrue(msg, duration.matches("[0-9]* mn"));
+	}
 }
